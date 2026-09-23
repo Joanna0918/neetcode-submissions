3a586c2c750623b67a20ad608c3ae26f1d68ac69
Yucheng Jiang <99860778+Joanna0918@users.noreply.github.com>
2026-09-23 11:56:06 -0400

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        alreadyMerge = False
+        for interval in intervals:
+            if alreadyMerge:
+                res.append(interval)
+                continue
+            
+            if interval[1] < newInterval[0]:
+                res.append(interval)
+            elif interval[0] > newInterval[1]:
+                res.append(newInterval)
+                res.append(interval)
+                alreadyMerge = True
+            else:
+                newInterval = [min(interval[0], newInterval[0]), max(interval[1], newInterval[1])]
+        
+        if not alreadyMerge:
+            res.append(newInterval)
+        return res
+
+
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res = []
+
+        for interval in intervals:
+            if not res:
+                res.append(interval)
+            else:
+                res = self.insert(res, interval)
+        
+        return res
